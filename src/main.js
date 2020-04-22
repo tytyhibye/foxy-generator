ab2d42b4e70fc65cf230473009d34ef594e2a791
Tyler Bates <Fondlytyler@gmail.com>
2020-04-22 16:29:06 -0700

    further explore fetching without async for second API.

--- src/main.js
+++ src/main.js
@@ -1,33 +1,49 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
-import $ from "jquery";
-import { Foxy } from './image.js';
-export { Wizzy } from './name.js';
+import $ from 'jquery';
+import {Foxy} from './image.js';
+import {Wizzy} from './name.js';
 
 $(document).ready(function () {
   $('#foxButton').click(function () {
     (async () => {
       let foxy = new Foxy();
       const response = await foxy.getFoxImage();
-      // console.log('response: ', response);
+      console.log('response: ', response);
       getElements(response);
     })();
     function getElements(response) {
       if (response) {
         let imageLink = response.image;
-        // let name = response[1].name; 
+        // let name = response[1].name;
         // $().html(`<img src=${imageLink} />`)
         $('.foxImage').html(`<img src=${imageLink}></img>`);
       } else {
-        $('.foxImage').text('Uh oh, something went wrong. Please refresh and try again.')
+        $('.foxImage').text(
+          'Uh oh, something went wrong. Please refresh and try again.'
+        );
       }
     }
-    $('#foxButton').fadeOut(300);
-    $('#wizButton').fadeIn(1500);
-  }
-  
-//   });
+  });
+  // $('#foxButton').fadeOut(300);
+  // $('#wizButton').fadeIn(1500);
+  $('#wizButton').click(function () {
+    let wizzy = new Wizzy();
+    const response = wizzy.getWizName();
+    getElements(response);
+    console.log('response: ', response);
+    function getElements(response) {
+      if (response) {
+        let index = wizzy.randomNumber(parseInt(response.length));
+        let nameLink = response[index].name;
+        console.log(index);
+        console.log(nameLink);
+        $('.showName').text(nameLink);
+      }
+      // wizzy.randomNumber(response.length)
+    }
+  });
 });
 
 // $2a$10$BU9J1FskYFj9Tr2aWZ5Rw.3oU/34XYSJhNMGNUBF01RL7u2VTXWVa
--- src/name.js
+++ src/name.js
@@ -1,25 +1,25 @@
 export class Wizzy {
   getWizName() { 
-    fetch(`https://www.potterapi.com/v1/characters?key=${process.env.API_KEY}`)
-    .then(function(response) {
-      if (response.ok && response.status == 200) {
-        return response.json();
-      } else {
+    console.log('process.env.API_KEY: ', process.env.API_KEY);
+    fetch(`https://www.potterapi.com/v1/characters?key=${process.env.API_KEY}`) 
+      .then(function(response) {
+        if (response.ok && response.status == 200) {
+          return response.json();
+        } else {
+          return false;
+        }
+      })
+      .catch(function(error) {
         return false;
-      }
-    })
-    .catch(function(error) {
-      return false;
-    })
-    .then(function(jsonifiedResponse) {
-      getElements(jsonifiedResponse);
-    });
+      })
+      .then(function(jsonifiedResponse) {
+        return jsonifiedResponse;
+      });
   }  
   randomNumber(max) {
     return Math.floor(Math.random() * Math.floor(max));
   }
 }
 
-// console.log('process.env.API_KEY: ', process.env.API_KEY);
 
 
