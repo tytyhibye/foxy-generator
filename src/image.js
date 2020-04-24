2e1c1b6413bdbc79e81563dad6e67b37e377e1b8
Tyler Bates <Fondlytyler@gmail.com>
2020-04-23 20:36:05 -0700

    refactor business logic.

--- src/image.js
+++ src/image.js
@@ -1,23 +1,22 @@
 export class Foxy {
   async getFoxImage() {
    
+    let jsonifiedResponse;
     try {
       let response = await fetch(`https://randomfox.ca/floof/`);
       // console.log('i am past the await');
       
-      let jsonifiedResponse;
       if (response.ok && response.status == 200) {
         jsonifiedResponse = await response.json();
-
+          
       } else {
         jsonifiedResponse = false;
       }
-      return jsonifiedResponse;
     } catch (error) {
       // console.log('in the catch');
-      
       return false;
-    }    
+    }  
+    return jsonifiedResponse;  
   }
 
   
